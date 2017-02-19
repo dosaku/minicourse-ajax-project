92e52f6669229b2766b778ad7c1d9dbd2e9eadeb
Ryan Minniear <rjminniear@gmail.com>
2017-02-19 00:38:35 -0800

    feat: add street view on input + button click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 
+NYTkey = 'd6102fe0fcc8476dadd38d660ab1d619';
+
+function loadStreetView(address) {
+
+    var baseURL = 'http://maps.googleapis.com/maps/api/streetview?';
+    var size = 'size=600x400';
+    var location = 'location=' + address
+    var streetViewURL = baseURL + size + '&' + location;
+    $('.bgimg').attr('src', streetViewURL);
+}
+
 function loadData() {
 
     var $body = $('body');
@@ -7,11 +18,16 @@ function loadData() {
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
 
+    var address = $('#street').val() + ', ' + $('#city').val();
+    console.log(address);
+
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
+    $greeting.text('So, you want to live at ' + address + '?');
+    loadStreetView(address);
+
 
     // YOUR CODE GOES HERE!
 
